5b1da6e9cf02cf0f467f7420b138cfc8c22bbd3d
Leonardo <leonardopicolloto@gmail.com>
2024-02-11 19:45:11 -0400

    feat: adjust navigation

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,35 +1,31 @@
 import React, { useContext } from "react";
-// import { getApps, initializeApp } from "firebase/app";
 import { AuthContext } from "../provider/AuthProvider";
 
 import { NavigationContainer } from "@react-navigation/native";
 
-import Main from "./MainStack";
-import Auth from "./AuthStack";
 import Loading from "../screens/utils/Loading";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CitySelection from "../screens/CitySelection";
+import Login from "../screens/Login";
 
-// Better put your these secret keys in .env file
-// const firebaseConfig = {
-//   apiKey: "",
-//   authDomain: "",
-//   databaseURL: "",
-//   projectId: "",
-//   storageBucket: "",
-//   messagingSenderId: "",
-//   appId: "",
-// };
-// if (getApps().length === 0) {
-//   initializeApp(firebaseConfig);
-// }
+const Stack = createNativeStackNavigator();
 
 export default () => {
   const auth = useContext(AuthContext);
-  const user = auth.user;
+  const { user, city } = auth;
+
   return (
     <NavigationContainer>
       {user == null && <Loading />}
-      {user == false && <Auth />}
-      {user == true && <Main />}
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName={!user ? "CitySelection" : "Login"}
+      >
+        <Stack.Screen name="CitySelection" component={CitySelection} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
