f5631d88f33874b849b23ea667b1ac230b762d69
Leonardo <leonardopicolloto@gmail.com>
2024-02-11 19:43:54 -0400

    feat: create types and add logo

--- src/types/navigation.tsx
+++ src/types/navigation.tsx
@@ -1,16 +1,14 @@
 export type MainStackParamList = {
-	MainTabs: undefined;
-	SecondScreen: undefined;
+  MainTabs: undefined;
+  SecondScreen: undefined;
 };
 
 export type AuthStackParamList = {
-	Login: undefined;
-	Register: undefined;
-	ForgetPassword: undefined;
+  Login: undefined;
 };
 
 export type MainTabsParamList = {
-	Home: undefined;
-	Profile: undefined;
-	About: undefined;
+  Home: undefined;
+  Profile: undefined;
+  About: undefined;
 };
